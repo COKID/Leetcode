f1c4715e05184453e6d260ef7f7711924a651cc0
COKID <COKIDCC@outlook.com>
2019-03-10 22:42:07 +0800

    2019-3-10（DP）

---
+++ 303.区域和检索-数组不可变.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=java
+ *
+ * [303] 区域和检索 - 数组不可变
+ *
+ * https://leetcode-cn.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (50.17%)
+ * Total Accepted:    7.8K
+ * Total Submissions: 15K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * 
+ * 示例：
+ * 
+ * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * 
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 
+ * 说明:
+ * 
+ * 
+ * 你可以假设数组不可变。
+ * 会多次调用 sumRange 方法。
+ * 
+ * 
+ */
+class NumArray {
+
+    public int[] nums;
+    public NumArray(int[] nums) {
+        this.nums=nums;
+    }
+    
+    public int sumRange(int i, int j) {
+        int sum=0;
+        for(int k=i;k<=j;k++){
+            sum+=nums[k];
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
+
